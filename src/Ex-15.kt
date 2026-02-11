4fa59bc2c47774b1c00eddc64bfa35a25257690a
giovana-lua <giovanabarbie8@gmail.com>
2026-02-10 23:33:34 -0300

    ex-15

--- src/Ex-15.kt
+++ src/Ex-15.kt
@@ -1,4 +1,21 @@
 fun main() {
-    var idade = listOf(12,18,3,24,5,10,19)
+    val idade = listOf(12,18,3,24,5,10,19)
 
+        val filtrado = idade.filter { it >= 18}
+
+    var soma = 0.0
+    var quantidade = 0
+
+
+    for ( n in filtrado) {
+        soma += n
+        quantidade ++
+    }
+
+   if (quantidade > 0) {
+       val media = soma / quantidade
+       println("Média $media")
+   }else {
+       println("Nenhum número foi encontrado")
+   }
 }
